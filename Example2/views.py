0207175c4d06425ed164ddb66f03d7fffe62f79e
Rajakuruva <kuruvarajasekhar2019@gmail.com>
2023-03-07 11:13:43 +0530

    first commit

---
+++ Example2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def Radha(request):
+    return HttpResponse("Hi i am Radha Function")
+def Krishna(request):
+    return HttpResponse("Hi i am Radha lover krishna")
